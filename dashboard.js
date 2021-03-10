2e5c2529195c3b165560d83edd2da90a148e54aa
Jether <31163547+jethers@users.noreply.github.com>
2021-03-10 20:38:36 -0300

    Update dashboard.js
    Sugestão de utilização do método get do objeto URLSearchParams ao invés de usar o método substr da String.

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,9 @@
 function recuperarDashboard(){
     var str = window.location.search;
     console.log("parametro da URL = "+str);
+    const urlParams = new URLSearchParams(str);
 
-    var idAgente = str.substr(4);
+    //var idAgente = str.substr(4);
+    var idAgente = urlParams.get("id");
     console.log("ID do Agente = "+idAgente);
 }
